a83e36f1dc91dd07e60cdcbe251f76d90134c40c
Daniel Fortich <fortichdaniel16@gmail.com>
2024-11-24 10:25:06 -0500

    Authentication

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    public function store(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'The credentials are incorrect'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return response()->json([
+            'data' => [
+                'attributes' => [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email
+                ],
+                'token' => $user->createToken($request->device_name)->plainTextToken
+            ]
+        ], Response::HTTP_OK);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\RecipeController;
 use App\Http\Controllers\Api\TagController;
 
@@ -10,10 +11,14 @@ use App\Http\Controllers\Api\TagController;
     return $request->user();
 }); */
 
-Route::get('categories',                [CategoryController::class, 'index']);
-Route::get('categories/{category}',     [CategoryController::class, 'show']);
+Route::post('login', [LoginController::class, 'store']);
 
-Route::apiResource('recipes', RecipeController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('categories',                [CategoryController::class, 'index']);
+    Route::get('categories/{category}',     [CategoryController::class, 'show']);
 
-Route::get('tags',                      [TagController::class, 'index']);
-Route::get('tags/{tag}',                [TagController::class, 'show']);
+    Route::apiResource('recipes', RecipeController::class);
+
+    Route::get('tags',                      [TagController::class, 'index']);
+    Route::get('tags/{tag}',                [TagController::class, 'show']);
+});
