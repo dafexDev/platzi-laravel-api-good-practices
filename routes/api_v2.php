5f08c8df0c9d7ca09a02e8e56a909a2715d092c9
Daniel Fortich <fortichdaniel16@gmail.com>
2024-11-25 07:13:49 -0500

    Version 2

--- routes/api_v2.php
+++ routes/api_v2.php
@@ -1,3 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\V2\RecipeController;
+use Illuminate\Support\Facades\Route;
 
+Route::prefix('v2')->group(function () {
+    Route::get('recipes', [RecipeController::class, 'index']);
+});
---
+++ tests/Feature/Http/Controllers/Api/V2/RecipeTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api\V2;
+
+use App\Models\Category;
+use App\Models\Recipe;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
+
+class RecipeTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_index()
+    {
+        Sanctum::actingAs(User::factory()->create());
+
+        Category::factory()->create();
+
+        Recipe::factory(5)->create();
+
+        $response = $this->getJson('/api/v2/recipes');
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJsonCount(5, 'data')
+            ->assertJsonStructure([
+                'data'  => [],
+                'links' => [],
+                'meta'  => [],
+            ]);
+    }
+}
